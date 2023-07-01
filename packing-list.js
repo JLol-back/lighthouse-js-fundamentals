2fb6b667b30f76eab0f9dfdff304332e3b2f6c99
Jake Lollback <jalollback@gmail.com>
2023-07-01 15:26:47 -0500

    secondary commit - while loop

---
+++ packing-list.js
@@ -0,0 +1,21 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+// Modified to use a while loop
+
+console.log("Kitchen stuff to pack:");
+let i = 0
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
+
+
+// Initial code from exercise
+
+console.log("Kitchen stuff to pack:");
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
+
+packingList[3];
+
